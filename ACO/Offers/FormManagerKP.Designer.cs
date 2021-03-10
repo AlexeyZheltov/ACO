0bac1bc13f26c52258ce5a5d141494bb57cc6353
Алексей Евсеев <evseev_aleksey93@mail.com>
2021-03-11 01:26:32 +0300

    Форма Диспетчер КП . Дизайн

--- ACO/Offers/FormManagerKP.Designer.cs
+++ ACO/Offers/FormManagerKP.Designer.cs
@@ -41,25 +41,28 @@
             this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TableColumns = new ACO.ProjectManager.CustomDataGrid();
             this.BtnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.TableColumns)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnAddColumns
             // 
-            this.BtnAddColumns.Location = new System.Drawing.Point(406, 12);
+            this.BtnAddColumns.Location = new System.Drawing.Point(7, 207);
             this.BtnAddColumns.Name = "BtnAddColumns";
-            this.BtnAddColumns.Size = new System.Drawing.Size(75, 23);
+            this.BtnAddColumns.Size = new System.Drawing.Size(190, 23);
             this.BtnAddColumns.TabIndex = 2;
-            this.BtnAddColumns.Text = "Обновить";
+            this.BtnAddColumns.Text = "Добавить выделенные ячейки";
             this.BtnAddColumns.UseVisualStyleBackColor = true;
             this.BtnAddColumns.Click += new System.EventHandler(this.BtnAddColumns_Click);
             // 
             // BtnSave
             // 
-            this.BtnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnSave.Location = new System.Drawing.Point(736, 460);
+            this.BtnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnSave.Location = new System.Drawing.Point(329, 207);
             this.BtnSave.Name = "BtnSave";
-            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.Size = new System.Drawing.Size(95, 23);
             this.BtnSave.TabIndex = 2;
             this.BtnSave.Text = "Сохранить";
             this.BtnSave.UseVisualStyleBackColor = true;
@@ -67,12 +70,15 @@
             // 
             // ListKP
             // 
+            this.ListKP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.ListKP.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.ColName});
             this.ListKP.HideSelection = false;
-            this.ListKP.Location = new System.Drawing.Point(13, 38);
+            this.ListKP.Location = new System.Drawing.Point(6, 60);
             this.ListKP.Name = "ListKP";
-            this.ListKP.Size = new System.Drawing.Size(468, 115);
+            this.ListKP.Size = new System.Drawing.Size(410, 120);
             this.ListKP.TabIndex = 3;
             this.ListKP.UseCompatibleStateImageBehavior = false;
             this.ListKP.View = System.Windows.Forms.View.Details;
@@ -81,20 +87,23 @@
             // ColName
             // 
             this.ColName.Text = "Настройки  КП";
-            this.ColName.Width = 448;
+            this.ColName.Width = 310;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(59, 25);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(296, 20);
+            this.textBox1.Size = new System.Drawing.Size(258, 20);
             this.textBox1.TabIndex = 4;
             // 
             // BtnCreate
             // 
-            this.BtnCreate.Location = new System.Drawing.Point(315, 11);
+            this.BtnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnCreate.Location = new System.Drawing.Point(323, 23);
             this.BtnCreate.Name = "BtnCreate";
-            this.BtnCreate.Size = new System.Drawing.Size(85, 23);
+            this.BtnCreate.Size = new System.Drawing.Size(93, 23);
             this.BtnCreate.TabIndex = 5;
             this.BtnCreate.Text = "Добавить";
             this.BtnCreate.UseVisualStyleBackColor = true;
@@ -133,7 +142,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TableColumns.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.TableColumns.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-            this.TableColumns.Location = new System.Drawing.Point(13, 159);
+            this.TableColumns.Location = new System.Drawing.Point(7, 236);
             this.TableColumns.MultiSelect = false;
             this.TableColumns.Name = "TableColumns";
             this.TableColumns.RowHeadersVisible = false;
@@ -141,38 +150,63 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.TableColumns.RowsDefaultCellStyle = dataGridViewCellStyle1;
             this.TableColumns.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.TableColumns.Size = new System.Drawing.Size(798, 295);
+            this.TableColumns.Size = new System.Drawing.Size(421, 220);
             this.TableColumns.TabIndex = 1;
             this.TableColumns.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.TableColumns_CellValueChanged);
             // 
             // BtnDelete
             // 
-            this.BtnDelete.Location = new System.Drawing.Point(487, 12);
+            this.BtnDelete.Location = new System.Drawing.Point(203, 207);
             this.BtnDelete.Name = "BtnDelete";
-            this.BtnDelete.Size = new System.Drawing.Size(85, 23);
+            this.BtnDelete.Size = new System.Drawing.Size(120, 23);
             this.BtnDelete.TabIndex = 6;
-            this.BtnDelete.Text = "Удалить";
+            this.BtnDelete.Text = "Удалить строку";
             this.BtnDelete.UseVisualStyleBackColor = true;
             this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.ListKP);
+            this.groupBox1.Controls.Add(this.BtnCreate);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 186);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки КП";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Новый";
             // 
             // FormManagerKP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 490);
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.BtnDelete);
-            this.Controls.Add(this.BtnCreate);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.ListKP);
             this.Controls.Add(this.BtnSave);
             this.Controls.Add(this.BtnAddColumns);
             this.Controls.Add(this.TableColumns);
+            this.MinimumSize = new System.Drawing.Size(450, 500);
             this.Name = "FormManagerKP";
-            this.Text = "FormManagerKP";
+            this.ShowIcon = false;
+            this.Text = "Диспетчер КП";
             this.Load += new System.EventHandler(this.FormManagerKP_Load);
             ((System.ComponentModel.ISupportInitialize)(this.TableColumns)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -189,5 +223,7 @@
         private System.Windows.Forms.Button BtnCreate;
         private System.Windows.Forms.ColumnHeader ColName;
         private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
